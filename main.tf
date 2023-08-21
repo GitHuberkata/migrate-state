5691a30bd5521a5cc3be7bd41c530ea759f0d631
GitHuberkata <petyagavrilova86@gmail.com>
2023-08-21 09:22:50 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.63.0"
+    }
+  }
+}
+provider "aws" {
+    region = "us-west-1"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+data "aws_key_pair" "search_for_key_pair" {}
+resource "aws_instance" "VM" {
+  count = 3
+  instance_type = var.ec2_type
+  ami = data.aws_ami.ubuntu.id
+  key_name = data.aws_key_pair.search_for_key_pair.key_name
+tags = {
+  Name = "testVMs"
+}
+}
